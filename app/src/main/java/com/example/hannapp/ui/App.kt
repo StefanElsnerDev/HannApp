c40b46d4be0c1924d524a3dc36268188a4c8ee5f
Stefan Elsner <stefan.elsner@aracom.de>
2023-02-19 14:20:52 +0100

    introduce QuantityInput and Button

--- app/src/main/java/com/example/hannapp/ui/App.kt
+++ app/src/main/java/com/example/hannapp/ui/App.kt
@@ -1,11 +1,24 @@
 package com.example.hannapp.ui
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Check
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -21,6 +34,26 @@ fun App() {
         ) {
             LandScapeSelectionScreen()
         }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun QuantityInput(modifier: Modifier) {
+    var text by rememberSaveable{ mutableStateOf("") }
+
+    OutlinedTextField(
+        value = text,
+        onValueChange = { text = it },
+        label = { Text("Quantity") },
+        modifier = modifier
+    )
+}
+
+@Composable
+fun Button(){
+    IconButton(onClick = { /*TODO*/ }) {
+        Icon(Icons.Filled.Check,"")
     }
 }
 
@@ -42,7 +75,18 @@ fun LandScapeSelectionScreen() {
                         .align(Alignment.CenterVertically)
                         .weight(0.5f)
                 )
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.SpaceEvenly,
+                    modifier = Modifier
+                    .fillMaxSize()
+                    .weight(0.5f)) {
+                    QuantityInput(
+                        modifier = Modifier.wrapContentSize()
+                    )
+                    Button()
                 }
             }
         }
     }
+}
