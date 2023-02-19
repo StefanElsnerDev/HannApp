50b5305f58b9eb86c55e565b093d78d683a03a55
Stefan Elsner <stefan.elsner@aracom.de>
2023-02-19 14:35:40 +0100

    introduce PortraitSelectionScreen

--- app/src/main/java/com/example/hannapp/ui/App.kt
+++ app/src/main/java/com/example/hannapp/ui/App.kt
@@ -1,9 +1,11 @@
 package com.example.hannapp.ui
 
+import android.content.res.Configuration
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Check
@@ -21,6 +23,7 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.hannapp.ui.selection.DropDown
 import com.example.hannapp.ui.theme.HannAppTheme
@@ -32,7 +35,10 @@ fun App() {
             modifier = Modifier.fillMaxSize(),
             color = MaterialTheme.colorScheme.background
         ) {
-            LandScapeSelectionScreen()
+            when(LocalConfiguration.current.orientation){
+                Configuration.ORIENTATION_LANDSCAPE -> LandScapeSelectionScreen()
+                else -> PortraitSelectionScreen()
+            }
         }
     }
 }
@@ -51,8 +57,10 @@ fun QuantityInput(modifier: Modifier) {
 }
 
 @Composable
-fun Button(){
-    IconButton(onClick = { /*TODO*/ }) {
+fun Button(modifier: Modifier){
+    IconButton(
+        modifier = modifier,
+        onClick = { /*TODO*/ }) {
         Icon(Icons.Filled.Check,"")
     }
 }
@@ -79,13 +87,53 @@ fun LandScapeSelectionScreen() {
                     horizontalAlignment = Alignment.CenterHorizontally,
                     verticalArrangement = Arrangement.SpaceEvenly,
                     modifier = Modifier
-                    .fillMaxSize()
-                    .weight(0.5f)) {
+                        .fillMaxSize()
+                        .weight(0.5f)) {
                     QuantityInput(
                         modifier = Modifier.wrapContentSize()
                     )
-                    Button()
+                    Button(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .weight(0.1f)
+                    )
                 }
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true,
+    device = "spec:width=1280dp,height=800dp,dpi=240,orientation=portrait"
+)
+@Composable
+fun PortraitSelectionScreen() {
+    HannAppTheme {
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colorScheme.background
+        ) {
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.SpaceEvenly,
+                modifier = Modifier
+                    .fillMaxSize()
+            ) {
+                DropDown(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .weight(0.8f)
+                )
+                QuantityInput(
+                    modifier = Modifier
+                        .wrapContentSize()
+                        .weight(0.1f)
+                )
+                Button(
+                    modifier = Modifier
+                        .wrapContentSize()
+                        .weight(0.1f)
+                )
             }
         }
     }
