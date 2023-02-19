8d754629c635c351be2f998ac148e71396fabd4d
Stefan Elsner <stefan.elsner@aracom.de>
2023-02-19 14:17:03 +0100

    introduce LandScapeSelectionScreen

--- app/src/main/java/com/example/hannapp/ui/App.kt
+++ app/src/main/java/com/example/hannapp/ui/App.kt
@@ -1,11 +1,14 @@
 package com.example.hannapp.ui
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import com.example.hannapp.ui.theme.HannAppTheme
 
 @Composable
@@ -15,7 +18,24 @@ fun App() {
             modifier = Modifier.fillMaxSize(),
             color = MaterialTheme.colorScheme.background
         ) {
-            Text("HannApp")
+            LandScapeSelectionScreen()
+        }
+    }
+}
+
+@Preview(showBackground = true, device = "spec:width=1280dp,height=800dp,dpi=240")
+@Composable
+fun LandScapeSelectionScreen() {
+    HannAppTheme {
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colorScheme.background
+        ) {
+            Row(
+                horizontalArrangement = Arrangement.SpaceEvenly,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+            }
         }
     }
 }
