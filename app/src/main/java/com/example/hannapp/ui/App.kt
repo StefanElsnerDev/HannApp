bf961babd4eed901df38aa60726a4017940e2da5
Stefan Elsner <stefan.elsner@aracom.de>
2023-02-27 09:55:22 +0100

    avoid code duplication by refactoring PortraitSelectionScreen

--- app/src/main/java/com/example/hannapp/ui/App.kt
+++ app/src/main/java/com/example/hannapp/ui/App.kt
@@ -6,21 +6,13 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Check
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalConfiguration
@@ -58,27 +50,7 @@ fun LandScapeSelectionScreen() {
                 horizontalArrangement = Arrangement.SpaceEvenly,
                 verticalAlignment = Alignment.CenterVertically
             ) {
-                DropDown(
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .align(Alignment.CenterVertically)
-                        .weight(0.5f)
-                )
-                Column(
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                    verticalArrangement = Arrangement.SpaceEvenly,
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .weight(0.5f)) {
-                    QuantityInput(
-                        modifier = Modifier.wrapContentSize()
-                    )
-                    Button(
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .weight(0.1f)
-                    )
-                }
+                PortraitSelectionScreen()
             }
         }
     }
@@ -89,33 +61,25 @@ fun LandScapeSelectionScreen() {
 )
 @Composable
 fun PortraitSelectionScreen() {
-    HannAppTheme {
-        Surface(
-            modifier = Modifier.fillMaxSize(),
-            color = MaterialTheme.colorScheme.background
-        ) {
-            Column(
-                horizontalAlignment = Alignment.CenterHorizontally,
-                verticalArrangement = Arrangement.SpaceEvenly,
-                modifier = Modifier
-                    .fillMaxSize()
-            ) {
-                DropDown(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .weight(0.8f)
-                )
-                QuantityInput(
-                    modifier = Modifier
-                        .wrapContentSize()
-                        .weight(0.1f)
-                )
-                Button(
-                    modifier = Modifier
-                        .wrapContentSize()
-                        .weight(0.1f)
-                )
-            }
-        }
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.SpaceBetween,
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(32.dp)
+    ) {
+        DropDown(
+            modifier = Modifier
+                .fillMaxWidth()
+        )
+        QuantityInput(
+            modifier = Modifier
+                .wrapContentSize()
+        )
+        Button(
+            modifier = Modifier
+                .wrapContentSize(),
+            icon = Icons.Filled.Add
+        )
     }
 }
