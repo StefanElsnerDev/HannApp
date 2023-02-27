ee39682fba00bf3df931c5adf40bcfa4bb45c7ca
Stefan Elsner <stefan.elsner@aracom.de>
2023-02-27 13:48:04 +0100

    introduce calculation screen for results of malto addition and milk rejection

---
+++ app/src/main/java/com/example/hannapp/ui/output/CalculationResult.kt
@@ -0,0 +1,56 @@
+package com.example.hannapp.ui.output
+
+import android.content.res.Configuration
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.hannapp.R
+import com.example.hannapp.ui.theme.HannAppTheme
+
+@Composable
+fun CalculationScreen(){
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.SpaceEvenly,
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(8.dp)
+    ) {
+        OutputCard(modifier = Modifier.fillMaxWidth(), drawable = R.drawable.liter, text = "1234.56 ml", label = "Milchausschuss")
+        OutputCard(modifier = Modifier.fillMaxWidth(), drawable = R.drawable.spoon, text = "1234.56 g", label = "Maltozugabe")
+
+    }
+}
+
+@Preview(
+    backgroundColor = 0xFFD0BCFF,
+    heightDp = 800,
+    showBackground = true,
+    uiMode = Configuration.UI_MODE_NIGHT_YES
+)
+@Composable
+fun CalculationPreview_DarkMode(){
+    HannAppTheme {
+        CalculationScreen()
+    }
+}
+
+@Preview(
+    backgroundColor = 0xFFD0BCFF,
+    heightDp = 800,
+    showBackground = true,
+    uiMode = Configuration.UI_MODE_NIGHT_NO
+)
+@Composable
+fun CalculationPreview_LightMode(){
+    HannAppTheme {
+        CalculationScreen()
+    }
+}
