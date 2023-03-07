2dca1c442cade85d8e10fb211d5005f265a89122
Stefan Elsner <stefan.elsner@aracom.de>
2023-03-07 14:16:37 +0100

    [wip] transform DropDown to use Dialog

--- app/src/main/java/com/example/hannapp/ui/selection/DropDown.kt
+++ app/src/main/java/com/example/hannapp/ui/selection/DropDown.kt
@@ -1,10 +1,27 @@
 package com.example.hannapp.ui.selection
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.IntrinsicSize
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.text.ClickableText
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowDropDown
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -13,36 +30,84 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Dialog
+import kotlin.math.exp
 
+@OptIn(ExperimentalMaterial3Api::class)
+@Preview(showBackground = true)
 @Composable
-fun DropDown(items: List<String> = listOf("A", "B", "C", "D", "E", "F"),
-             modifier: Modifier
+fun DropDown(
+    modifier: Modifier = Modifier,
+    dings: List<String> = List(100){it.toString()}
 ) {
-    var expanded by rememberSaveable{ mutableStateOf(false) }
+    var expanded by rememberSaveable { mutableStateOf(false) }
     var selectedIndex by rememberSaveable { mutableStateOf(0) }
 
-    Box(modifier = modifier) {
-        ClickableText(
-            text = AnnotatedString(items[selectedIndex]),
-            modifier = Modifier.fillMaxWidth().align(Alignment.Center))
-        {
-            expanded = true
-        }
-        DropdownMenu(
-            expanded = expanded,
-            onDismissRequest = { expanded = false },
+    Box(modifier = modifier.height(IntrinsicSize.Min)) {
+        OutlinedTextField(
+            value = dings[selectedIndex],
+            onValueChange = {},
             modifier = Modifier
                 .fillMaxWidth()
-        ) {
-            items.forEachIndexed { index, s ->
-                DropdownMenuItem(
-                    onClick = {
-                        selectedIndex = index
-                        expanded = false
-                    },
-                    text = { Text(text = s) })
+                .align(Alignment.Center),
+            readOnly = true,
+            textStyle = MaterialTheme.typography.titleMedium,
+            label = { Text(text = "Auswahl") },
+            trailingIcon = {
+                if (!expanded) Icon(Icons.Filled.ArrowDropDown, contentDescription = null)
+            },
+        )
+        Surface(
+            //ClickBox
+            modifier = Modifier
+                .fillMaxSize()
+                .clickable { expanded = true },
+            color = Color.Transparent,
+        ) {}
+
+        if (expanded) {
+            Dialog(onDismissRequest = { expanded = false }) {
+                val listState = rememberLazyListState()
+                LazyColumn(modifier = Modifier.fillMaxWidth(), state = listState) {
+//                    if (notSetLabel != null) {
+//                        item {
+//                            LargeDropdownMenuItem(
+//                                text = notSetLabel,
+//                                selected = false,
+//                                enabled = false,
+//                                onClick = { },
+//                            )
+//                        }
+//                    }
+                    itemsIndexed(dings) { index, item ->
+                        Text(text = item)
+                        //val selectedItem = index == selectedIndex
+                    }
+                }
             }
         }
     }
 }
+
+//        DropdownMenu(
+//            expanded = expanded,
+//            onDismissRequest = { expanded = false },
+//            modifier = Modifier
+//                .fillMaxWidth()
+//        ) {
+//            items.forEachIndexed { index, s ->
+//                DropdownMenuItem(
+//                    onClick = {
+//                        selectedIndex = index
+//                        expanded = false
+//                    },
+//                    text = { Text(
+//                        style = MaterialTheme.typography.titleMedium,
+//                        text = s
+//                    ) })
+//            }
+//        }
--- app/src/main/java/com/example/hannapp/ui/theme/Theme.kt
+++ app/src/main/java/com/example/hannapp/ui/theme/Theme.kt
@@ -12,6 +12,7 @@ import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
@@ -26,17 +27,16 @@ private val DarkColorScheme = darkColorScheme(
 private val LightColorScheme = lightColorScheme(
     primary = Purple40,
     secondary = PurpleGrey40,
-    tertiary = Pink40
+    tertiary = Pink40,
 
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
+    /* Other default colors to override */
+    background = Pink80,
+    surface = Pink40,
     onPrimary = Color.White,
     onSecondary = Color.White,
     onTertiary = Color.White,
     onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+    onSurface = Pink80,
 )
 
 @Composable
@@ -65,7 +65,7 @@ fun HannAppTheme(
     }
 
     MaterialTheme(
-        colorScheme = colorScheme,
+        colorScheme = LightColorScheme,
         typography = Typography,
         content = content
     )
