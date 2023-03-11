71f7b108df8bc57794dc05fc96dc8fbfa21aa2e3
Stefan Elsner <stefan.elsner.dev@gmail.com>
2023-03-11 13:28:14 +0100

    introduce composable AppScaffold

---
+++ app/src/main/java/com/example/hannapp/ui/components/AppScaffold.kt
@@ -0,0 +1,32 @@
+package com.example.hannapp.ui.components
+
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FabPosition
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun AppScaffold(
+    modifier: Modifier = Modifier,
+    topBar: @Composable (() -> Unit) = {},
+    bottomBar: @Composable (() -> Unit) = {},
+    floatingActionButton: @Composable (() -> Unit) = {},
+    floatingActionButtonPosition: FabPosition = FabPosition.End,
+    contentColor: Color = MaterialTheme.colorScheme.tertiary,
+    content: @Composable (PaddingValues) -> Unit
+) {
+    Scaffold(
+        modifier = modifier,
+        topBar = topBar,
+        bottomBar = bottomBar,
+        floatingActionButton = floatingActionButton,
+        floatingActionButtonPosition = floatingActionButtonPosition,
+        contentColor = contentColor,
+        content = content
+    )
+}
