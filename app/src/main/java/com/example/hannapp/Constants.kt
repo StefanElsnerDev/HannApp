0e1a8216a1d1bffc08e751ba3d2fe358bf1ae3fa
Stefan Elsner <stefan.elsner.dev@gmail.com>
2023-03-12 13:38:32 +0100

    introduce DatabaseModule for HILT DI

--- app/src/main/java/com/example/hannapp/Constants.kt
+++ app/src/main/java/com/example/hannapp/Constants.kt
@@ -1,6 +1,7 @@
 package com.example.hannapp
 
 object Constants {
+    const val DATABASE_NAME = "hannapp_database"
     const val NUTRITION_BMI_TABLE = "nutrition_BMI_table"
     const val NUTRITION_BMI_NAME = "nutrition_name"
 }
---
+++ app/src/main/java/com/example/hannapp/data/modul/DatabaseModule.kt
@@ -0,0 +1,27 @@
+package com.example.hannapp.data.modul
+
+import android.content.Context
+import androidx.room.Room
+import com.example.hannapp.Constants.DATABASE_NAME
+import com.example.hannapp.data.database.AppDatabase
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object DatabaseModule {
+
+    @Provides
+    @Singleton
+    fun provideDatabase(@ApplicationContext context: Context) = Room.databaseBuilder(
+        context, AppDatabase::class.java, DATABASE_NAME)
+        .build()
+
+    @Provides
+    @Singleton
+    fun provideNutritionBMIDao(db: AppDatabase) = db.nutritionBMIDao()
+}
