9b7f7ddcb597ac36fd88c18d3016232bc0f44b64
Stefan Elsner <stefan.elsner.dev@gmail.com>
2023-03-14 20:56:10 +0100

    remove dummy function from UseCase

--- app/src/main/java/com/example/hannapp/domain/GetNutritionBMIsUseCase.kt
+++ app/src/main/java/com/example/hannapp/domain/GetNutritionBMIsUseCase.kt
@@ -1,14 +1,10 @@
 package com.example.hannapp.domain
 
-import com.example.hannapp.data.model.entity.NutritionBMI
 import com.example.hannapp.data.repository.NutritionRepository
-import java.time.LocalDateTime
 import javax.inject.Inject
 
 class GetNutritionBMIsUseCase @Inject constructor(
     private val repository: NutritionRepository,
 ) {
     operator fun invoke() = repository.getNames()
-
-    suspend fun insertDummy() = repository.insert(NutritionBMI(name = LocalDateTime.now().toString()))
 }
