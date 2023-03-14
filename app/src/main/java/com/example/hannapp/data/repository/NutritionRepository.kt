50312f95e858815e644ee976a281ae18d7216f41
Stefan Elsner <stefan.elsner.dev@gmail.com>
2023-03-14 13:44:42 +0100

    remove suspendable for dao operations that return a flow

--- app/src/main/java/com/example/hannapp/data/repository/NutritionRepository.kt
+++ app/src/main/java/com/example/hannapp/data/repository/NutritionRepository.kt
@@ -2,39 +2,20 @@ package com.example.hannapp.data.repository
 
 import com.example.hannapp.data.database.dao.NutritionBMIDao
 import com.example.hannapp.data.model.entity.NutritionBMI
-import com.example.hannapp.data.modul.IoDispatcher
-import kotlinx.coroutines.CoroutineDispatcher
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 class NutritionRepository @Inject constructor(
     private val nutritionBMIDao: NutritionBMIDao,
-    @IoDispatcher private val dispatcher: CoroutineDispatcher = Dispatchers.IO
 ) {
-    suspend fun insert(nutritionBMI: NutritionBMI) =
-        withContext(dispatcher) {
-            nutritionBMIDao.insert(nutritionBMI)
-        }
+    suspend fun insert(nutritionBMI: NutritionBMI) = nutritionBMIDao.insert(nutritionBMI)
 
-    suspend fun get(nutritionName: String) =
-        withContext(dispatcher) {
-            nutritionBMIDao.getByName(nutritionName)
-        }
+    fun get(nutritionName: String) = nutritionBMIDao.getByName(nutritionName)
 
-    suspend fun getNames() =
-        withContext(dispatcher) {
-            nutritionBMIDao.getNames()
-        }
+    fun getNames() = nutritionBMIDao.getNames()
 
-    suspend fun update(nutritionBMI: NutritionBMI) =
-        withContext(dispatcher) {
-            nutritionBMIDao.update(nutritionBMI)
-        }
+    suspend fun update(nutritionBMI: NutritionBMI) = nutritionBMIDao.update(nutritionBMI)
 
-    suspend fun delete(nutritionBMI: NutritionBMI) =
-        withContext(dispatcher) {
-            nutritionBMIDao.delete(nutritionBMI)
-        }
+    suspend fun delete(nutritionBMI: NutritionBMI) = nutritionBMIDao.delete(nutritionBMI)
+
 }
 
