3dbaa4c28d47ae560bb9bfad6a175a8546a233df
Stefan Elsner <stefan.elsner.dev@gmail.com>
2023-03-28 09:09:03 +0200

    introduce NutritionModel for states

---
+++ app/src/main/java/com/example/hannapp/data/model/NutritionModel.kt
@@ -0,0 +1,13 @@
+package com.example.hannapp.data.model
+
+data class NutritionModel(
+    val name: String = "",
+    val kcal: String = "",
+    val protein: String = "",
+    val fad: String = "",
+    val carbohydrates: String = "",
+    val sugar: String = "",
+    val fiber: String = "",
+    val alcohol: String = "",
+    val energy: String = ""
+)
