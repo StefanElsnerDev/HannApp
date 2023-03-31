af013ddefa6bb45623cb01fa54355e7d8deda76c
Stefan Elsner <stefan.elsner.dev@gmail.com>
2023-03-31 14:40:00 +0200

    introduce api interface for search request

---
+++ app/src/main/java/com/example/hannapp/data/remote/ProductSearchApi.kt
@@ -0,0 +1,17 @@
+package com.example.hannapp.data.remote
+
+import com.example.hannapp.data.model.api.ProductSearchResult
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface ProductSearchApi {
+    @GET("search.pl")
+    suspend fun search(
+        @Query("search_terms") searchString: String,
+        @Query("page") page: Int,
+        @Query("json") json: Int = 1,
+        @Query("fields") fields: String = "code,product_name,nutriments",
+        @Query("page_size") pageSize: Int = 24
+    ): Response<ProductSearchResult>
+}
