30caac1ae92877586bd40b194ea331ce49d4e0d6
Stefan Elsner <stefan.elsner.dev@gmail.com>
2023-04-03 12:20:20 +0200

    change DropDownField to DropDownDialog with SimpleDropDownIcon

--- app/src/main/java/com/example/hannapp/ui/selection/Selection.kt
+++ app/src/main/java/com/example/hannapp/ui/selection/Selection.kt
@@ -1,84 +1,134 @@
 package com.example.hannapp.ui.selection
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.ArrowDropDown
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
+import androidx.paging.PagingData
+import androidx.paging.compose.LazyPagingItems
+import androidx.paging.compose.collectAsLazyPagingItems
 import com.example.hannapp.R
+import com.example.hannapp.data.model.entity.Nutrition
 import com.example.hannapp.ui.button.Button
 import com.example.hannapp.ui.components.AppScaffold
 import com.example.hannapp.ui.components.NavigationBar
+import com.example.hannapp.ui.dropdown.DropDownDialog
+import com.example.hannapp.ui.dropdown.SimpleDropDownItem
 import com.example.hannapp.ui.input.InputField
 import com.example.hannapp.ui.theme.HannAppTheme
 import com.example.hannapp.ui.viewmodel.NutritionUiState
 import com.example.hannapp.ui.viewmodel.NutritionViewModel
+import kotlinx.coroutines.flow.flowOf
 
-@Preview(device = "spec:width=1280dp,height=800dp,dpi=240,orientation=portrait")
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun SelectionContent(
-    uiState: NutritionUiState = NutritionUiState(),
-    onAdd: (String) -> Unit = {},
-    navController: NavHostController = rememberNavController(),
-    onItemSelected: (Int) -> Unit = {}
+    uiState: NutritionUiState,
+    pagingItems: LazyPagingItems<Nutrition>,
+    onAdd: (String) -> Unit,
+    navController: NavHostController,
+    onItemSelected: (String) -> Unit
 ) {
     val snackBarHost = remember { SnackbarHostState() }
 
-    HannAppTheme {
-        AppScaffold(
-            bottomBar = { NavigationBar(navController) },
-            snackBarHost = { SnackbarHost(hostState = snackBarHost) }
+    AppScaffold(
+        bottomBar = { NavigationBar(navController) },
+        snackBarHost = { SnackbarHost(hostState = snackBarHost) }
+    ) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.SpaceAround,
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(32.dp)
         ) {
-            Column(
-                horizontalAlignment = Alignment.CenterHorizontally,
-                verticalArrangement = Arrangement.SpaceAround,
+            var input by rememberSaveable { mutableStateOf("") }
+            var selectedItem by rememberSaveable { mutableStateOf("") }
+            var expanded by remember { mutableStateOf(false) }
+
+            uiState.errorMessage?.let {
+                val errorMessageText: String = uiState.errorMessage
+                val retryMessageText = stringResource(id = R.string.okay)
+
+                LaunchedEffect(errorMessageText, retryMessageText, snackBarHost) {
+                    snackBarHost.showSnackbar(
+                        message = errorMessageText,
+                        actionLabel = retryMessageText
+                    )
+                }
+            }
+
+            Box(
                 modifier = Modifier
-                    .fillMaxSize()
-                    .padding(32.dp)
+                    .height(IntrinsicSize.Min)
+                    .background(MaterialTheme.colorScheme.surface)
             ) {
-                var input by rememberSaveable{ mutableStateOf("") }
+                OutlinedTextField(
+                    value = selectedItem.ifBlank { "No Data" },
+                    onValueChange = {},
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .background(MaterialTheme.colorScheme.surface),
+                    readOnly = true,
+                    textStyle = MaterialTheme.typography.titleMedium,
+                    label = { Text(text = "Auswahl") },
+                    trailingIcon = {
+                        if (!expanded) Icon(Icons.Filled.ArrowDropDown, contentDescription = null)
+                    }
+                )
 
-                uiState.errorMessage?.let {
-                    val errorMessageText: String = uiState.errorMessage
-                    val retryMessageText = stringResource(id = R.string.okay)
+                //ClickBox
+                Surface(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .clickable { expanded = true },
+                    color = Color.Transparent,
+                ) {}
+            }
 
-                    LaunchedEffect(errorMessageText, retryMessageText, snackBarHost) {
-                        snackBarHost.showSnackbar(
-                            message = errorMessageText,
-                            actionLabel = retryMessageText
+            if (expanded) {
+                DropDownDialog(
+                    pagingItems = pagingItems,
+                    onDismiss = { expanded = false },
+                    itemContent = {
+                        SimpleDropDownItem(
+                            item = it,
+                            onClick = { item ->
+                                onItemSelected(item.toString())
+                                selectedItem = item.toString()
+                                expanded = false
+                            }
                         )
                     }
-                }
-
-                DropDownField(
-                    modifier = Modifier
-                        .fillMaxWidth(),
-                    items = uiState.foodList.map { it.toString() } //TODO Remove Develop Output
-                ) { onItemSelected(it) }
-                InputField(
-                    value = input,
-                    onValueChange = { input = it },
-                    modifier = Modifier
-                        .wrapContentSize(),
-                    label = stringResource(id = R.string.quantity),
-                    isError = false
                 )
-                Button(
-                    modifier = Modifier
-                        .wrapContentSize(),
-                    icon = Icons.Filled.Add
-                ) { onAdd(input) }
             }
+            InputField(
+                value = input,
+                onValueChange = { input = it },
+                modifier = Modifier
+                    .wrapContentSize(),
+                label = stringResource(id = R.string.quantity),
+                isError = false
+            )
+            Button(
+                modifier = Modifier
+                    .wrapContentSize(),
+                icon = Icons.Filled.Add
+            ) { onAdd(input) }
         }
     }
 }
@@ -93,8 +143,23 @@ fun SelectionScreen(
 
     SelectionContent(
         uiState = uiState,
+        pagingItems = flowOf(PagingData.from(listOf(Nutrition()))).collectAsLazyPagingItems(),
         onAdd = onAdd,
         navController = navController,
         onItemSelected = { }
     )
+}
+
+@Preview(device = "spec:width=1280dp,height=800dp,dpi=240,orientation=portrait")
+@Composable
+fun SelectionContent_LightMode() {
+    HannAppTheme {
+        SelectionContent(
+            uiState = NutritionUiState(),
+            pagingItems = flowOf(PagingData.from(listOf(Nutrition()))).collectAsLazyPagingItems(),
+            onAdd = {},
+            navController = rememberNavController(),
+            onItemSelected = {}
+        )
+    }
 }
