a449cb42c0ad8198a9528d02bf8e11bde6af89b8
Stefan Elsner <stefan.elsner.dev@gmail.com>
2023-04-04 09:45:09 +0200

    hoist states out of DropDownField and remove itemList

--- app/src/main/java/com/example/hannapp/ui/selection/DropDown.kt
+++ app/src/main/java/com/example/hannapp/ui/selection/DropDown.kt
@@ -1,197 +1,67 @@
 package com.example.hannapp.ui.selection
 
-import android.content.res.Configuration
-import androidx.compose.foundation.ExperimentalFoundationApi
+import android.content.res.Configuration.UI_MODE_NIGHT_NO
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.combinedClickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.itemsIndexed
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowDropDown
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import androidx.compose.ui.window.Dialog
-import androidx.compose.ui.window.DialogProperties
+import com.example.hannapp.data.model.NutritionModel
 import com.example.hannapp.ui.theme.HannAppTheme
 
 @OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun SimpleDropDownMenu(
+    modifier: Modifier = Modifier,
+    selected: String,
+    isExpanded: Boolean,
+    onClick: () -> Unit,
+) {
+    Box(
+        modifier = modifier
+            .height(IntrinsicSize.Min)
+            .background(MaterialTheme.colorScheme.surface)
+    ) {
+        OutlinedTextField(
+            value = selected.ifBlank { "No Data" },
+            onValueChange = {},
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(MaterialTheme.colorScheme.surface),
+            readOnly = true,
+            textStyle = MaterialTheme.typography.titleMedium,
+            label = { Text(text = "Auswahl") },
+            trailingIcon = {
+                if (!isExpanded) Icon(Icons.Filled.ArrowDropDown, contentDescription = null)
+            }
+        )
+        //ClickBox
+        Surface(
+            modifier = Modifier
+                .fillMaxSize()
+                .clickable { onClick() },
+            color = Color.Transparent,
+        ) {}
+    }
+}
+
 @Preview(
     showBackground = true,
     device = "spec:width=800dp,height=1280dp,dpi=240,orientation=portrait",
-    uiMode = Configuration.UI_MODE_NIGHT_NO)
+    uiMode = UI_MODE_NIGHT_NO
+)
 @Composable
-fun DropDownField(
-    modifier: Modifier = Modifier,
-    items: List<String> = List(1000) { it.toString() },
-    onItemSelected: (Int) -> Unit = {},
-    onDeleteSelected: (Int) -> Unit = {}
-) {
-    var expanded by remember { mutableStateOf(false) }
-    var selectedIndex by rememberSaveable { mutableStateOf(0) }
-
+fun DropDownField_LightMode() {
     HannAppTheme {
-        Box(
-            modifier = modifier
-                .height(IntrinsicSize.Min)
-                .background(MaterialTheme.colorScheme.surface)
-        ) {
-            OutlinedTextField(
-                value = if (items.isNotEmpty()) items[selectedIndex] else "No Data",
-                onValueChange = {},
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .align(Alignment.Center),
-                readOnly = true,
-                textStyle = MaterialTheme.typography.titleMedium,
-                label = { Text(text = "Auswahl") },
-                trailingIcon = {
-                    if (!expanded) Icon(Icons.Filled.ArrowDropDown, contentDescription = null)
-                },
-            )
-
-            //ClickBox
-            Surface(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .clickable { expanded = true },
-                color = Color.Transparent,
-            ) {}
-
-            if (expanded && items.isNotEmpty()) {
-                DropDownList(
-                    items = items,
-                    onItemSelected = {
-                        onItemSelected(it)
-                        selectedIndex = it
-                        expanded = false
-                    },
-                    onDeleteSelected = onDeleteSelected,
-                    onDismiss = { expanded = false }
-                )
-            }
-        }
+        SimpleDropDownMenu(
+            modifier = Modifier,
+            selected = NutritionModel().toString(),
+            isExpanded = false
+        ) {}
     }
 }
-
-@OptIn(ExperimentalComposeUiApi::class)
-@Preview(showBackground = true,
-    device = "spec:width=800dp,height=1280dp,dpi=240,orientation=portrait",
-    uiMode = Configuration.UI_MODE_NIGHT_NO)
-@Composable
-fun DropDownList(
-    modifier: Modifier = Modifier,
-    items: List<String> = List(100) { it.toString() },
-    onItemSelected: (Int) -> Unit = {},
-    onDeleteSelected: (Int) -> Unit = {},
-    onDismiss: () -> Unit = {}
-) {
-    Dialog(
-        properties = DialogProperties(usePlatformDefaultWidth = false),
-        onDismissRequest = onDismiss
-    ) {
-        HannAppTheme() {
-            Surface(
-                shape = RoundedCornerShape(12.dp)
-            ) {
-                LazyColumn(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(8.dp)
-                ) {
-                    itemsIndexed(items) { index, item ->
-                        //Text(text = item)
-                        DropDownItem(index = index,
-                            item = item,
-                            onClick = { onItemSelected(index) },
-                            onLongClick = { onDeleteSelected(index) }
-                        )
-                    }
-                }
-            }
-        }
-    }
-}
-
-@OptIn(ExperimentalFoundationApi::class)
-@Preview(showBackground = true,
-    device = "spec:width=800dp,height=1280dp,dpi=240,orientation=portrait",
-    uiMode = Configuration.UI_MODE_NIGHT_NO)
-@Composable
-fun DropDownItem(
-    modifier: Modifier = Modifier,
-    index: Int = 0,
-    item: String? = "Item",
-    onClick: (Int) -> Unit = {},
-    onLongClick: (Int) -> Unit = {}
-) {
-    HannAppTheme {
-        Box(
-            modifier = modifier
-                .fillMaxWidth()
-                .background(MaterialTheme.colorScheme.surface)
-                .padding(vertical = 4.dp)
-                .combinedClickable(
-                    onClick = { onClick(index) },
-                    onLongClick = { onLongClick(index) }
-                )
-        ) {
-            Text(
-                text = item ?: "empty",
-                fontSize = 24.sp,
-                color = MaterialTheme.colorScheme.onSurface,
-                style = MaterialTheme.typography.labelMedium
-            )
-        }
-    }
-}
-
-//            Dialog(onDismissRequest = { expanded = false }) {
-//                val listState = rememberLazyListState()
-//                LazyColumn(modifier = Modifier.fillMaxWidth(), state = listState) {
-////                    if (notSetLabel != null) {
-////                        item {
-////                            LargeDropdownMenuItem(
-////                                text = notSetLabel,
-////                                selected = false,
-////                                enabled = false,
-////                                onClick = { },
-////                            )
-////                        }
-////                    }
-//                    itemsIndexed(dings) { index, item ->
-//                        Text(text = item)
-//                        //val selectedItem = index == selectedIndex
-//                    }
-//                }
-//            }
-
-
-//        DropdownMenu(
-//            expanded = expanded,
-//            onDismissRequest = { expanded = false },
-//            modifier = Modifier
-//                .fillMaxWidth()
-//        ) {
-//            items.forEachIndexed { index, s ->
-//                DropdownMenuItem(
-//                    onClick = {
-//                        selectedIndex = index
-//                        expanded = false
-//                    },
-//                    text = { Text(
-//                        style = MaterialTheme.typography.titleMedium,
-//                        text = s
-//                    ) })
-//            }
-//        }
