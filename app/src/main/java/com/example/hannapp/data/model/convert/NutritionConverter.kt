9d5130cce20729042a9760a527fc6208222475cf
Stefan Elsner <stefan.elsner.dev@gmail.com>
2023-04-04 19:30:24 +0200

    introduce conversion between ui model and entity
    [toAmend] add conversion of NutritionUiModel to Enitity and vice versa
    
    [toAmend] add test case for conversion of NutritionUiModel

--- app/src/main/java/com/example/hannapp/data/model/convert/NutritionConverter.kt
+++ app/src/main/java/com/example/hannapp/data/model/convert/NutritionConverter.kt
@@ -1,12 +1,16 @@
 package com.example.hannapp.data.model.convert
 
 import com.example.hannapp.data.model.NutritionModel
+import com.example.hannapp.data.model.NutritionUiModel
 import com.example.hannapp.data.model.entity.Nutrition
 import javax.inject.Inject
 
 class NutritionConverter @Inject constructor() {
 
     fun model(nutritionModel: NutritionModel) = InnerNutritionModel(nutritionModel)
+
+    fun uiModel(nutritionUiModel: NutritionUiModel) = InnerNutritionUiModel(nutritionUiModel)
+
     fun entity(nutrition: Nutrition) = InnerNutrition(nutrition)
 
     class InnerNutritionModel(private val nutritionModel: NutritionModel) {
@@ -25,6 +29,26 @@ class NutritionConverter @Inject constructor() {
             )
         }
     }
+
+    class InnerNutritionUiModel(private val nutritionUiModel: NutritionUiModel) {
+
+        fun toEntity() = nutritionUiModel.let {
+
+            require(it.id != null){"Conversion failed due to missing entity ID"}
+
+            Nutrition(
+                uid = it.id,
+                name = it.name.ifBlank { null },
+                kcal = it.kcal.toDoubleOrNull(),
+                protein = it.protein.toDoubleOrNull(),
+                fat = it.fat.toDoubleOrNull(),
+                carbohydrates = it.carbohydrates.toDoubleOrNull(),
+                sugar = it.sugar.toDoubleOrNull(),
+                fiber = it.fiber.toDoubleOrNull(),
+                alcohol = it.alcohol.toDoubleOrNull()
+            )
+        }
+    }
     
     class InnerNutrition(private val nutrition: Nutrition){
         
@@ -39,6 +63,20 @@ class NutritionConverter @Inject constructor() {
                 sugar = it.sugar ?: 0.0,
                 fiber = it.fiber ?: 0.0,
                 alcohol = it.alcohol ?: 0.0
+            )
+        }
+
+        fun toUiModel() = nutrition.let {
+            NutritionUiModel(
+                id = it.uid,
+                name = it.name ?: "",
+                kcal = it.kcal?.toString() ?: "",
+                protein = it.protein?.toString() ?: "",
+                fat = it.fat?.toString() ?: "",
+                carbohydrates = it.carbohydrates?.toString() ?: "",
+                sugar = it.sugar?.toString() ?: "",
+                fiber = it.fiber?.toString() ?: "",
+                alcohol = it.alcohol?.toString() ?: "",
             )
         }
     }
--- app/src/test/java/com/example/hannapp/convert/NutritionConverterShould.kt
+++ app/src/test/java/com/example/hannapp/convert/NutritionConverterShould.kt
@@ -1,6 +1,7 @@
 package com.example.hannapp.convert
 
 import com.example.hannapp.data.model.NutritionModel
+import com.example.hannapp.data.model.NutritionUiModel
 import com.example.hannapp.data.model.convert.NutritionConverter
 import com.example.hannapp.data.model.entity.Nutrition
 import org.junit.jupiter.api.Assertions
@@ -11,8 +12,14 @@ class NutritionConverterShould {
 
     private lateinit var nutritionConverter: NutritionConverter
 
-    private val nutritionModel = NutritionModel(id = 123, name = "Apple", kcal = "1234.5")
-    private val nutritionEntity = Nutrition(uid = 123, name = "Apple", kcal = "1234.5")
+    private val id: Long = 123
+    private val name = "Apple"
+    private val kcal = 4.567
+    private val protein = 9.87
+
+    private val nutritionModel = NutritionModel(id = id, name = name, kcal = kcal, protein = protein)
+    private val nutritionEntity = Nutrition(uid = id, name = name, kcal = kcal, protein = protein)
+    private val nutritionUiModel = NutritionUiModel(id = id, name = name, kcal = kcal.toString(), protein = protein.toString())
 
     @BeforeEach
     fun beforeEach() {
@@ -31,5 +38,28 @@ class NutritionConverterShould {
         val result = nutritionConverter.entity(nutritionEntity).toModel()
 
         Assertions.assertEquals(nutritionModel, result)
+    }
+
+    @Test
+    fun convertUiNutritionModelToEntity(){
+        val result = nutritionConverter.uiModel(nutritionUiModel).toEntity()
+
+        Assertions.assertEquals(nutritionEntity, result)
+    }
+
+    @Test
+    fun throwOnMissingId(){
+       val incorrectNutritionUiModel = NutritionUiModel(name = name, kcal = kcal.toString(), protein = protein.toString())
+
+        Assertions.assertThrows(IllegalArgumentException::class.java){
+            nutritionConverter.uiModel(incorrectNutritionUiModel).toEntity()
+        }
+    }
+
+    @Test
+    fun convertEntityToUiNutritionModel(){
+        val result = nutritionConverter.entity(nutritionEntity).toUiModel()
+
+        Assertions.assertEquals(nutritionUiModel, result)
     }
 }
