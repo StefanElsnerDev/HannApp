c93c1154d15d26a57d5565e56cf06f47a40d6c0b
Stefan Elsner <stefan.elsner.dev@gmail.com>
2023-04-04 19:08:59 +0200

    introduce NutritionUiModel

---
+++ app/src/main/java/com/example/hannapp/data/model/NutritionUiModel.kt
@@ -0,0 +1,13 @@
+package com.example.hannapp.data.model
+
+data class NutritionUiModel(
+    val id: Long? = null,
+    val name: String = "",
+    val kcal: String = "",
+    val protein: String = "",
+    val fat: String = "",
+    val carbohydrates: String = "",
+    val sugar: String = "",
+    val fiber: String = "",
+    val alcohol: String = ""
+)
