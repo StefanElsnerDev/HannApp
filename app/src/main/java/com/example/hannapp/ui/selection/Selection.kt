caf7fe1570e3cdfc0937d883516c1591876200fc
Stefan Elsner <stefan.elsner.dev@gmail.com>
2023-04-05 15:46:26 +0200

    remove Scaffold from selection content

--- app/src/main/java/com/example/hannapp/ui/selection/Selection.kt
+++ app/src/main/java/com/example/hannapp/ui/selection/Selection.kt
@@ -15,158 +15,116 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.hilt.navigation.compose.hiltViewModel
-import androidx.navigation.NavHostController
-import androidx.navigation.compose.rememberNavController
 import androidx.paging.PagingData
 import androidx.paging.compose.LazyPagingItems
 import androidx.paging.compose.collectAsLazyPagingItems
 import com.example.hannapp.R
 import com.example.hannapp.data.model.entity.Nutrition
 import com.example.hannapp.ui.button.Button
-import com.example.hannapp.ui.components.AppScaffold
-import com.example.hannapp.ui.components.NavigationBar
 import com.example.hannapp.ui.dropdown.DropDownDialog
 import com.example.hannapp.ui.dropdown.SimpleDropDownItem
 import com.example.hannapp.ui.input.InputField
-import com.example.hannapp.ui.mood.Mood
-import com.example.hannapp.ui.output.CalculationScreen
 import com.example.hannapp.ui.theme.HannAppTheme
-import com.example.hannapp.ui.viewmodel.NutritionSelectViewModel
 import com.example.hannapp.ui.viewmodel.NutritionUiState
 import kotlinx.coroutines.flow.flowOf
 
-@OptIn(ExperimentalMaterial3Api::class)
+
 @Composable
+@OptIn(ExperimentalMaterial3Api::class)
 fun SelectionContent(
+    modifier: Modifier,
     uiState: NutritionUiState,
+    snackBarHost: SnackbarHostState,
+    onClickBoxClick: () -> Unit,
     pagingItems: LazyPagingItems<Nutrition>,
-    onAdd: (String) -> Unit,
-    navController: NavHostController,
-    onClickBoxClick: ()->Unit,
-    onItemSelected: (String) -> Unit
+    onItemSelected: (String) -> Unit,
+    onAdd: (String) -> Unit
 ) {
-    val snackBarHost = remember { SnackbarHostState() }
-
-    AppScaffold(
-        bottomBar = { NavigationBar(navController) },
-        snackBarHost = { SnackbarHost(hostState = snackBarHost) }
+    Column(
+        modifier = modifier.fillMaxSize().padding(32.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.SpaceAround
     ) {
-        Row(
-            modifier = Modifier.fillMaxSize(),
-            verticalAlignment = Alignment.Top,
-            horizontalArrangement = Arrangement.SpaceEvenly
-        ) {
-            Column(
-                modifier = Modifier.fillMaxWidth(0.5f).padding(32.dp),
-                horizontalAlignment = Alignment.CenterHorizontally,
-                verticalArrangement = Arrangement.SpaceAround
-            ) {
-                var input by rememberSaveable { mutableStateOf("") }
-                var selectedItem by rememberSaveable { mutableStateOf("") }
-                var expanded by remember { mutableStateOf(false) }
+        var input by rememberSaveable { mutableStateOf("") }
+        var selectedItem by rememberSaveable { mutableStateOf("") }
+        var expanded by remember { mutableStateOf(false) }
 
-                uiState.errorMessage?.let {
-                    val errorMessageText: String = uiState.errorMessage
-                    val retryMessageText = stringResource(id = R.string.okay)
+        uiState.errorMessage?.let {
+            val errorMessageText: String = it
+            val retryMessageText = stringResource(id = R.string.okay)
 
-                    LaunchedEffect(errorMessageText, retryMessageText, snackBarHost) {
-                        snackBarHost.showSnackbar(
-                            message = errorMessageText,
-                            actionLabel = retryMessageText
-                        )
-                    }
-                }
-
-                Box(
-                    modifier = Modifier
-                        .height(IntrinsicSize.Min)
-                        .background(MaterialTheme.colorScheme.surface)
-                ) {
-                    OutlinedTextField(
-                        value = selectedItem.ifBlank { "No Data" },
-                        onValueChange = {},
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .background(MaterialTheme.colorScheme.surface),
-                        readOnly = true,
-                        textStyle = MaterialTheme.typography.titleMedium,
-                        label = { Text(text = "Auswahl") },
-                        trailingIcon = {
-                            if (!expanded) Icon(
-                                Icons.Filled.ArrowDropDown,
-                                contentDescription = null
-                            )
-                        }
-                    )
-
-                    //ClickBox
-                    Surface(
-                        modifier = Modifier
-                            .fillMaxSize()
-                            .clickable {
-                                onClickBoxClick()
-                                expanded = true
-                            },
-                        color = Color.Transparent,
-                    ) {}
-                }
-
-                if (expanded) {
-                    DropDownDialog(
-                        pagingItems = pagingItems,
-                        onDismiss = { expanded = false },
-                        itemContent = {
-                            SimpleDropDownItem(
-                                item = it,
-                                onClick = { item ->
-                                    onItemSelected(item.toString())
-                                    selectedItem = item.toString()
-                                    expanded = false
-                                }
-                            )
-                        }
-                    )
-                }
-                InputField(
-                    value = input,
-                    onValueChange = { input = it },
-                    modifier = Modifier
-                        .wrapContentSize(),
-                    label = stringResource(id = R.string.quantity),
-                    isError = false
+            LaunchedEffect(errorMessageText, retryMessageText, snackBarHost) {
+                snackBarHost.showSnackbar(
+                    message = errorMessageText,
+                    actionLabel = retryMessageText
                 )
-                Button(
-                    modifier = Modifier
-                        .wrapContentSize(),
-                    icon = Icons.Filled.Add
-                ) { onAdd(input) }
             }
-            CalculationScreen(
-                modifier = Modifier.fillMaxWidth(0.5f).padding(32.dp),
-                mood = Mood.GREEN
+        }
+
+        Box(
+            modifier = Modifier
+                .height(IntrinsicSize.Min)
+                .background(MaterialTheme.colorScheme.surface)
+        ) {
+            OutlinedTextField(
+                value = selectedItem.ifBlank { "No Data" },
+                onValueChange = {},
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .background(MaterialTheme.colorScheme.surface),
+                readOnly = true,
+                textStyle = MaterialTheme.typography.titleMedium,
+                label = { Text(text = "Auswahl") },
+                trailingIcon = {
+                    if (!expanded) Icon(
+                        Icons.Filled.ArrowDropDown,
+                        contentDescription = null
+                    )
+                }
+            )
+
+            //ClickBox
+            Surface(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .clickable {
+                        onClickBoxClick()
+                        expanded = true
+                    },
+                color = Color.Transparent,
+            ) {}
+        }
+
+        if (expanded) {
+            DropDownDialog(
+                pagingItems = pagingItems,
+                onDismiss = { expanded = false },
+                itemContent = {
+                    SimpleDropDownItem(
+                        item = it,
+                        onClick = { item ->
+                            onItemSelected(item.toString())
+                            selectedItem = item.toString()
+                            expanded = false
+                        }
+                    )
+                }
             )
         }
+        InputField(
+            value = input,
+            onValueChange = { input = it },
+            modifier = Modifier
+                .wrapContentSize(),
+            label = stringResource(id = R.string.quantity),
+            isError = false
+        )
+        Button(
+            modifier = Modifier
+                .wrapContentSize(),
+            icon = Icons.Filled.Add
+        ) { onAdd(input) }
     }
-}
-
-@Composable
-fun SelectionScreen(
-    viewModel: NutritionSelectViewModel = hiltViewModel(),
-    onAdd: (String) -> Unit = {},
-    navController: NavHostController
-) {
-    val uiState by viewModel.uiState.collectAsState()
-    val nutriments = viewModel.nutriments.collectAsLazyPagingItems()
-
-    SelectionContent(
-        uiState = uiState,
-        pagingItems = nutriments,
-        onAdd = onAdd,
-        navController = navController,
-        onClickBoxClick = { viewModel.getAll() },
-        onItemSelected = { }
-    )
 }
 
 @Preview(device = "spec:width=1280dp,height=800dp,dpi=240,orientation=landscape")
@@ -174,12 +132,13 @@ fun SelectionScreen(
 fun SelectionContent_LightMode() {
     HannAppTheme {
         SelectionContent(
+            modifier = Modifier,
             uiState = NutritionUiState(),
+            snackBarHost = SnackbarHostState(),
             pagingItems = flowOf(PagingData.from(listOf(Nutrition()))).collectAsLazyPagingItems(),
-            onAdd = {},
             onClickBoxClick = {},
-            navController = rememberNavController(),
-            onItemSelected = {}
+            onItemSelected = {},
+            onAdd = {}
         )
     }
 }
