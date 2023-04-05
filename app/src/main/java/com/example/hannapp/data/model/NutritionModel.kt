1f82ab24eff0c89b29aef6f41419382c7ff13efe
Stefan Elsner <stefan.elsner.dev@gmail.com>
2023-04-05 09:05:05 +0200

    make nutrition model properties nullable

--- app/src/main/java/com/example/hannapp/data/model/NutritionModel.kt
+++ app/src/main/java/com/example/hannapp/data/model/NutritionModel.kt
@@ -2,12 +2,12 @@ package com.example.hannapp.data.model
 
 data class NutritionModel(
     val id: Long? = null,
-    val name: String = "",
-    val kcal: Double  = 0.0,
-    val protein: Double  = 0.0,
-    val fat: Double  = 0.0,
-    val carbohydrates: Double  = 0.0,
-    val sugar: Double  = 0.0,
-    val fiber: Double  = 0.0,
-    val alcohol: Double  = 0.0
+    val name: String? = null,
+    val kcal: Double? = null,
+    val protein: Double? = null,
+    val fat: Double? = null,
+    val carbohydrates: Double? = null,
+    val sugar: Double? = null,
+    val fiber: Double? = null,
+    val alcohol: Double? = null
 )
