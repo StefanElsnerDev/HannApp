9bff6fc793d2f77c3f3e870c7308a0445fc93974
Stefan Elsner <stefan.elsner.dev@gmail.com>
2023-04-08 22:13:29 +0200

    adapt color of facial expression to theme

--- app/src/main/java/com/example/hannapp/ui/mood/MoodLight.kt
+++ app/src/main/java/com/example/hannapp/ui/mood/MoodLight.kt
@@ -8,26 +8,25 @@ import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.hannapp.R
 import com.example.hannapp.ui.theme.HannAppTheme
 
-enum class Mood(val value: String, val color: Color, @DrawableRes val drawable: Int){
-    RED("concerned", Color.Red, R.drawable.concerned),
-    YELLOW("neutral", Color.Yellow, R.drawable.neutral),
-    GREEN("satisfied", Color.Green, R.drawable.smiling)
+enum class Mood(val value: String, @DrawableRes val drawable: Int){
+    RED("concerned", R.drawable.concerned),
+    YELLOW("neutral", R.drawable.neutral),
+    GREEN("satisfied", R.drawable.smiling)
 }
 
 @Composable
 fun MoodLight(mood: Mood){
     Surface(
         modifier = Modifier.clip(RoundedCornerShape(12.dp)),
-        color = MaterialTheme.colorScheme.surface
+        color = MaterialTheme.colorScheme.surfaceVariant
     ) {
-        Icon(painter = painterResource(id = mood.drawable), contentDescription = mood.value, tint = mood.color)
+        Icon(painter = painterResource(id = mood.drawable), contentDescription = mood.value, tint = MaterialTheme.colorScheme.onSurfaceVariant)
     }
 }
 
