e91fe8164c7d3cda7790f747176ecd463e166983
Stefan Elsner <stefan.elsner.dev@gmail.com>
2023-04-08 22:15:33 +0200

    remove surface of facial expressions with regard to merge into CalculationContent

--- app/src/main/java/com/example/hannapp/ui/mood/MoodLight.kt
+++ app/src/main/java/com/example/hannapp/ui/mood/MoodLight.kt
@@ -1,38 +1,32 @@
 package com.example.hannapp.ui.mood
 
 import androidx.annotation.DrawableRes
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import com.example.hannapp.R
 import com.example.hannapp.ui.theme.HannAppTheme
 
-enum class Mood(val value: String, @DrawableRes val drawable: Int){
+enum class Mood(val value: String, @DrawableRes val drawable: Int) {
     RED("concerned", R.drawable.concerned),
     YELLOW("neutral", R.drawable.neutral),
     GREEN("satisfied", R.drawable.smiling)
 }
 
 @Composable
-fun MoodLight(mood: Mood){
-    Surface(
-        modifier = Modifier.clip(RoundedCornerShape(12.dp)),
-        color = MaterialTheme.colorScheme.surfaceVariant
-    ) {
-        Icon(painter = painterResource(id = mood.drawable), contentDescription = mood.value, tint = MaterialTheme.colorScheme.onSurfaceVariant)
-    }
+fun MoodLight(mood: Mood) {
+    Icon(
+        painter = painterResource(id = mood.drawable),
+        contentDescription = mood.value,
+        tint = MaterialTheme.colorScheme.onSurfaceVariant
+    )
 }
 
 @Preview
 @Composable
-fun NeutralMood_Light(){
+fun NeutralMood_Light() {
     HannAppTheme {
         MoodLight(mood = Mood.RED)
     }
@@ -40,7 +34,7 @@ fun NeutralMood_Light(){
 
 @Preview
 @Composable
-fun HappyMoodLight_Light(){
+fun HappyMoodLight_Light() {
     HannAppTheme {
         MoodLight(mood = Mood.YELLOW)
     }
@@ -48,7 +42,7 @@ fun HappyMoodLight_Light(){
 
 @Preview
 @Composable
-fun VerySatisfiedMoodLight_Light(){
+fun VerySatisfiedMoodLight_Light() {
     HannAppTheme {
         MoodLight(mood = Mood.GREEN)
     }
