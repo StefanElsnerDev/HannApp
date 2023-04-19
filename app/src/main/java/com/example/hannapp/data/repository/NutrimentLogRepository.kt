ac626b469645cdfab561ffe97bf0b4a541c7c92f
Stefan Elsner <stefan.elsner.dev@gmail.com>
2023-04-19 10:52:57 +0200

    introduce repository for handling logs
    [toAmend] fix repo

---
+++ app/src/main/java/com/example/hannapp/data/repository/NutrimentLogRepository.kt
@@ -0,0 +1,25 @@
+package com.example.hannapp.data.repository
+
+import com.example.hannapp.data.database.dao.NutrimentLogDao
+import com.example.hannapp.data.model.NutrimentLogModel
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+import javax.inject.Inject
+
+class NutrimentLogRepository @Inject constructor(
+    private val nutrimentLogDao: NutrimentLogDao
+) {
+    suspend fun log(nutrimentLogModel: NutrimentLogModel) = nutrimentLogDao.log(nutrimentLogModel)
+
+    fun getLogs(): Flow<List<NutrimentLogModel>> =
+        nutrimentLogDao.getLogs().map { logList ->
+            logList.map {
+                NutrimentLogModel(
+                    nutrition = it.nutrition,
+                    quantity = it.nutrimentLog.quantity,
+                    createdAt = it.nutrimentLog.createdAt,
+                    modifiedAt = it.nutrimentLog.lastModifiedAt
+                )
+            }
+        }
+}
