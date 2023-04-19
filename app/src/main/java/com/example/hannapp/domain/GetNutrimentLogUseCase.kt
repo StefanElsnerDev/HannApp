b9f1d6d8e1d49e8551ae969b3e81ea9a83eec199
Stefan Elsner <stefan.elsner.dev@gmail.com>
2023-04-19 10:53:45 +0200

    implement logic of use case for getting logs via repository

--- app/src/main/java/com/example/hannapp/domain/GetNutrimentLogUseCase.kt
+++ app/src/main/java/com/example/hannapp/domain/GetNutrimentLogUseCase.kt
@@ -1,11 +1,12 @@
 package com.example.hannapp.domain
 
 import com.example.hannapp.data.model.NutrimentLogModel
+import com.example.hannapp.data.repository.NutrimentLogRepository
 import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
-class GetNutrimentLogUseCase @Inject constructor() {
-    fun observeNutrimentLog(): Flow<List<NutrimentLogModel>>{
-        TODO()
-    }
+class GetNutrimentLogUseCase @Inject constructor(
+    private val nutrimentLogRepository: NutrimentLogRepository
+) {
+    fun observeNutrimentLog(): Flow<List<NutrimentLogModel>> = nutrimentLogRepository.getLogs()
 }
