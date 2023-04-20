73ec51151918b42bf83a740caf5836e8dd63da36
Stefan Elsner <stefan.elsner.dev@gmail.com>
2023-04-20 16:35:32 +0200

    implement use case for insert nutriment log entry

--- app/src/main/java/com/example/hannapp/domain/InsertNutrimentLogUseCase.kt
+++ app/src/main/java/com/example/hannapp/domain/InsertNutrimentLogUseCase.kt
@@ -1,8 +1,14 @@
 package com.example.hannapp.domain
 
 import com.example.hannapp.data.model.NutrimentLogModel
+import com.example.hannapp.data.repository.NutrimentLogRepository
 import javax.inject.Inject
 
-class InsertNutrimentLogUseCase @Inject constructor() {
-    suspend operator fun invoke(nutrimentLogModel: NutrimentLogModel): Boolean = false
+class InsertNutrimentLogUseCase @Inject constructor(
+    private val nutrimentLogRepository: NutrimentLogRepository
+) {
+    suspend operator fun invoke(nutrimentLogModel: NutrimentLogModel): Boolean{
+        val index = nutrimentLogRepository.log(nutrimentLogModel)
+        return index != -1L
+    }
 }
