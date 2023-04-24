20e59ef5d979782c76fd5877a604adbad5cab5ff
Stefan Elsner <stefan.elsner.dev@gmail.com>
2023-04-24 13:53:44 +0200

    scroll to bottom of history column automatically

--- app/src/main/java/com/example/hannapp/ui/history/NutrimentHistoryContent.kt
+++ app/src/main/java/com/example/hannapp/ui/history/NutrimentHistoryContent.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.hannapp.data.model.NutrimentUiLogModel
@@ -29,6 +30,11 @@ fun NutrimentHistoryContent(
             .wrapContentHeight(),
         shape = MaterialTheme.shapes.medium
     ) {
+        LaunchedEffect(nutriments) {
+            if (listState.layoutInfo.totalItemsCount - 1 < nutriments.size)
+                listState.animateScrollToItem(nutriments.size)
+        }
+
         LazyColumn(
             modifier = Modifier.background(MaterialTheme.colorScheme.surfaceVariant),
             state = listState
