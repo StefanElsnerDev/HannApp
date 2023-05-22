5dd1c9e9cf97b92132b2b1d022e53a88870eb0c3
Stefan Elsner <stefan.elsner.dev@gmail.com>
2023-05-22 14:04:02 +0200

    introduce enum for window sizes

---
+++ app/src/main/java/com/example/hannapp/utils/WindowSize.kt
@@ -0,0 +1,9 @@
+package com.example.hannapp.utils
+
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+
+enum class WindowSize(val size: Dp) {
+    COMPACT(480.dp),
+    MEDIUM(900.dp),
+}
