6bc2c99e7596b1609b970eca5186ca2ffbac7c6d
Stefan Elsner <stefan.elsner.dev@gmail.com>
2023-05-22 12:54:19 +0200

    [toAmend] windowsclass to MainActivity

--- app/src/main/java/com/example/hannapp/MainActivity.kt
+++ app/src/main/java/com/example/hannapp/MainActivity.kt
@@ -3,15 +3,20 @@ package com.example.hannapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.material3.windowsizeclass.ExperimentalMaterial3WindowSizeClassApi
+import androidx.compose.material3.windowsizeclass.calculateWindowSizeClass
 import com.example.hannapp.ui.App
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3WindowSizeClassApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            App()
+            App(
+                widthSizeClass = calculateWindowSizeClass(this@MainActivity).widthSizeClass
+            )
         }
     }
 }
