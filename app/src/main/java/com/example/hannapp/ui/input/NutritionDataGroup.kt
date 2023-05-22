85389f6f824e2b33fbc8ad901c1be5a7e6daa08f
Stefan Elsner <stefan.elsner.dev@gmail.com>
2023-05-22 08:58:12 +0200

    implement keyboard action of input field in nutrition data group

--- app/src/main/java/com/example/hannapp/ui/input/NutritionDataGroup.kt
+++ app/src/main/java/com/example/hannapp/ui/input/NutritionDataGroup.kt
@@ -5,16 +5,29 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
-import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.lazy.grid.itemsIndexed
+import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusDirection
+import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.hannapp.R
-import com.example.hannapp.data.distinct.*
+import com.example.hannapp.data.distinct.Alcohol
+import com.example.hannapp.data.distinct.Carbohydrates
+import com.example.hannapp.data.distinct.Fat
+import com.example.hannapp.data.distinct.Fiber
+import com.example.hannapp.data.distinct.Kcal
+import com.example.hannapp.data.distinct.NutritionComponent
+import com.example.hannapp.data.distinct.NutritionDataComponent
+import com.example.hannapp.data.distinct.Protein
+import com.example.hannapp.data.distinct.Sugar
 import com.example.hannapp.data.model.NutritionUiModel
 import com.example.hannapp.ui.mapComponentToModelProperty
 import com.example.hannapp.ui.theme.Constraints.PADDING
@@ -33,13 +46,21 @@ fun NutritionDataGroup(
         modifier = Modifier.padding(PADDING),
         shape = MaterialTheme.shapes.medium
     ) {
+        val localFocusManager = LocalFocusManager.current
+
         LazyVerticalGrid(
             modifier = Modifier.background(MaterialTheme.colorScheme.surfaceVariant).padding(PADDING),
             columns = GridCells.Adaptive(300.dp),
         ) {
-
-            items(uiComponents) { component ->
-
+            itemsIndexed(uiComponents) { index, component ->
+                val imeAction =
+                    if (index == uiComponents.lastIndex) ImeAction.Done else ImeAction.Next
+                val keyboardActions = KeyboardActions(
+                    onNext = {
+                        localFocusManager.moveFocus(FocusDirection.Next)
+                    },
+                    onDone = { TODO("action on last item") },
+                )
                 val isError = errors.contains(component.type) && showErrors
 
                 InputField(
@@ -55,7 +76,10 @@ fun NutritionDataGroup(
                         stringResource(id = R.string.fill_field)
                     } else {
                         ""
-                    }
+                    },
+                    keyboardType = KeyboardType.Number,
+                    keyboardActions = keyboardActions,
+                    imeAction = imeAction,
                 )
             }
         }
