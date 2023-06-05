5ab15b3e25e3896637455680ae6bd010feb211e9
Stefan Elsner <stefan.elsner.dev@gmail.com>
2023-06-05 11:46:28 +0200

    emit milk ui references model when collecting from MilkReferenceRepository

---
+++ app/src/main/java/com/example/hannapp/data/model/MilkReferenceUiModel.kt
@@ -0,0 +1,8 @@
+package com.example.hannapp.data.model
+
+data class MilkReferenceUiModel(
+    val maxQuantity: Float,
+    val dayTimeQuantity: Float,
+    val preNightQuantity: Float,
+    val nightQuantity: Float
+)
--- app/src/main/java/com/example/hannapp/data/repository/MilkReferenceRepository.kt
+++ app/src/main/java/com/example/hannapp/data/repository/MilkReferenceRepository.kt
@@ -1,7 +1,10 @@
 package com.example.hannapp.data.repository
 
 import com.example.hannapp.data.model.MilkReferenceModel
+import com.example.hannapp.data.model.MilkReferenceUiModel
 import com.example.hannapp.data.source.MilkReferenceDataSource
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 
 class MilkReferenceRepository @Inject constructor(
@@ -28,5 +31,18 @@ class MilkReferenceRepository @Inject constructor(
         )
     }
 
-    fun emitReference() = milkReferenceDataSource.emitReferences()
+    fun emitReference(): Flow<MilkReferenceUiModel> =
+        milkReferenceDataSource.emitReferences().map {
+            require(it.maxQuantity != null)
+            require(it.dayTimeQuantity != null)
+            require(it.preNightQuantity != null)
+            require(it.nightQuantity != null)
+
+            MilkReferenceUiModel(
+                maxQuantity = it.maxQuantity,
+                dayTimeQuantity = it.dayTimeQuantity,
+                preNightQuantity = it.preNightQuantity,
+                nightQuantity = it.nightQuantity
+            )
+        }
 }
