f5ecb87a88738ed52dbc2a211c4f663cb4c86957
Stefan Elsner <stefan.elsner.dev@gmail.com>
2023-06-05 11:45:59 +0200

    emit nutrition ui references model when collecting from NutritionReferenceRepository

--- app/src/main/java/com/example/hannapp/data/repository/NutritionReferenceRepository.kt
+++ app/src/main/java/com/example/hannapp/data/repository/NutritionReferenceRepository.kt
@@ -1,7 +1,10 @@
 package com.example.hannapp.data.repository
 
 import com.example.hannapp.data.model.NutritionReferences
+import com.example.hannapp.data.model.NutritionUiReferences
 import com.example.hannapp.data.source.NutritionReferenceDataSource
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 
 class NutritionReferenceRepository @Inject constructor(
@@ -28,5 +31,18 @@ class NutritionReferenceRepository @Inject constructor(
         )
     }
 
-    fun emitReference() = nutritionReferenceDataSource.emitReferences()
+    fun emitReference(): Flow<NutritionUiReferences> =
+        nutritionReferenceDataSource.emitReferences().map {
+            require(it.kcal != null)
+            require(it.protein != null)
+            require(it.carbohydrates != null)
+            require(it.fat != null)
+
+            NutritionUiReferences(
+                kcal = it.kcal,
+                protein = it.protein,
+                carbohydrates = it.carbohydrates,
+                fat = it.fat
+            )
+        }
 }
