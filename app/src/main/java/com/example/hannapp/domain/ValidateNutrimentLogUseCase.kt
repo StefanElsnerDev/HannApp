ff44e6f8992241dd3f2257253f326acf203cc175
Stefan Elsner <stefan.elsner.dev@gmail.com>
2023-06-11 14:30:53 +0200

    introduce ValidateNutrimentLogUseCase

---
+++ app/src/main/java/com/example/hannapp/domain/ValidateNutrimentLogUseCase.kt
@@ -0,0 +1,12 @@
+package com.example.hannapp.domain
+
+import com.example.hannapp.data.repository.NutrimentLogValidationRepository
+import com.example.hannapp.ui.mood.Mood
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class ValidateNutrimentLogUseCase @Inject constructor(
+    private val nutrimentLogValidationRepository: NutrimentLogValidationRepository
+) {
+    operator fun invoke(): Flow<Mood> = nutrimentLogValidationRepository.validate()
+}
---
+++ app/src/test/java/com/example/hannapp/usecase/ValidateNutrimentLogUseCaseShould.kt
@@ -0,0 +1,48 @@
+package com.example.hannapp.usecase
+
+import com.example.hannapp.data.repository.NutrimentLogValidationRepository
+import com.example.hannapp.domain.ValidateNutrimentLogUseCase
+import com.example.hannapp.ui.mood.Mood
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.test.runTest
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.mockito.kotlin.mock
+import org.mockito.kotlin.verify
+import org.mockito.kotlin.whenever
+
+@OptIn(ExperimentalCoroutinesApi::class)
+class ValidateNutrimentLogUseCaseShould {
+
+    private lateinit var validateNutrimentLogUseCase: ValidateNutrimentLogUseCase
+    private val nutrimentLogValidationRepository = mock<NutrimentLogValidationRepository>()
+
+    @BeforeEach
+    fun beforeEach() {
+        whenever(nutrimentLogValidationRepository.validate()).thenReturn(
+            flowOf(Mood.GREEN)
+        )
+
+        validateNutrimentLogUseCase = ValidateNutrimentLogUseCase(
+            nutrimentLogValidationRepository = nutrimentLogValidationRepository
+        )
+    }
+
+    @Test
+    fun invokeValidationOfNutritionLimitRepository() = runTest {
+        validateNutrimentLogUseCase.invoke()
+
+        verify(nutrimentLogValidationRepository).validate()
+    }
+
+    @Test
+    fun emitMoodOnLogBelowLimit() = runTest {
+        val mood = validateNutrimentLogUseCase.invoke().first()
+
+        assertThat(mood).isInstanceOf(Mood::class.java)
+        assertThat(mood).isEqualTo(Mood.GREEN)
+    }
+}
