ca7ff7e7adf5fef658f78bfc7d1c24e74b5c6bde
Stefan Elsner <stefan.elsner.dev@gmail.com>
2023-06-11 14:19:49 +0200

    introduce NutrimentLogValidationRepository

---
+++ app/src/main/java/com/example/hannapp/data/repository/NutrimentLogValidationRepository.kt
@@ -0,0 +1,48 @@
+package com.example.hannapp.data.repository
+
+import com.example.hannapp.data.model.NutrimentLogModel
+import com.example.hannapp.data.model.NutritionLimitReferenceModel
+import com.example.hannapp.ui.mood.Mood
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.combine
+import javax.inject.Inject
+
+class NutrimentLogValidationRepository @Inject constructor(
+    private val nutritionLimitsRepository: NutritionLimitsRepository,
+    private val nutrimentLogRepository: NutrimentLogRepository
+) {
+    fun validate(): Flow<Mood> = nutrimentLogRepository.getLogs()
+        .combine(nutritionLimitsRepository.getDailyShare()) { logModels, limit ->
+
+            val levels = calcNutritionLevels(logModels, limit)
+
+            when {
+                levels.any { it >= 1 } -> Mood.RED
+                levels.any { it >= 0.8f } -> Mood.YELLOW
+                else -> Mood.GREEN
+            }
+        }
+
+    private fun calcLevel(quantities: List<Double>, limit: Double): Float =
+        quantities.sumOf { it }.div(limit).toFloat()
+
+    private fun calcNutritionLevels(
+        logModels: List<NutrimentLogModel>,
+        limit: NutritionLimitReferenceModel
+    ): List<Float> {
+        return listOf(
+            calcLevel(
+                quantities = logModels.map { it.nutrition.protein ?: 0.0 },
+                limit = limit.protein
+            ),
+            calcLevel(
+                quantities = logModels.map { it.nutrition.carbohydrates ?: 0.0 },
+                limit = limit.carbohydrates
+            ),
+            calcLevel(
+                quantities = logModels.map { it.nutrition.fat ?: 0.0 },
+                limit = limit.fat
+            )
+        )
+    }
+}
---
+++ app/src/test/java/com/example/hannapp/repository/NutrimentLogValidationRepositoryShould.kt
@@ -0,0 +1,151 @@
+package com.example.hannapp.repository
+
+import com.example.hannapp.data.model.NutrimentLogModel
+import com.example.hannapp.data.model.NutritionLimitReferenceModel
+import com.example.hannapp.data.model.entity.Nutrition
+import com.example.hannapp.data.repository.NutrimentLogRepository
+import com.example.hannapp.data.repository.NutrimentLogValidationRepository
+import com.example.hannapp.data.repository.NutritionLimitsRepository
+import com.example.hannapp.ui.mood.Mood
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.test.runTest
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.mockito.kotlin.mock
+import org.mockito.kotlin.whenever
+
+@OptIn(ExperimentalCoroutinesApi::class)
+class NutrimentLogValidationRepositoryShould {
+
+    private lateinit var nutrimentLogValidationRepository: NutrimentLogValidationRepository
+    private val nutritionLimitsRepository = mock<NutritionLimitsRepository>()
+    private val nutrimentLogRepository = mock<NutrimentLogRepository>()
+
+    private val nutritionLimit = NutritionLimitReferenceModel(
+        kcal = 10.0,
+        protein = 10.0,
+        carbohydrates = 10.0,
+        fat = 10.0
+    )
+
+    private fun generateNutrimentLevelLog(
+        limit: NutritionLimitReferenceModel,
+        level: Float,
+        logSize: Int
+    ) = (1..logSize).map { index ->
+        NutrimentLogModel(
+            id = index.toLong(),
+            nutrition = Nutrition(
+                uid = index.toLong(),
+                name = "",
+                kcal = (level * limit.kcal).div(logSize),
+                protein = (level * limit.protein).div(logSize),
+                fat = (level * limit.fat).div(logSize),
+                carbohydrates = (level * limit.carbohydrates).div(logSize)
+            ),
+            quantity = 1.0,
+            createdAt = 1234567,
+            modifiedAt = null
+        )
+    }
+
+    @BeforeEach
+    fun beforeEach() {
+        whenever(nutritionLimitsRepository.getDailyShare()).thenReturn(
+            flowOf(nutritionLimit)
+        )
+
+        nutrimentLogValidationRepository = NutrimentLogValidationRepository(
+            nutritionLimitsRepository = nutritionLimitsRepository,
+            nutrimentLogRepository = nutrimentLogRepository
+        )
+    }
+
+    @Test
+    fun emitGreenMoodOnEmptyNutriments() = runTest {
+        whenever(nutrimentLogRepository.getLogs()).thenReturn(
+            flowOf(
+                generateNutrimentLevelLog(
+                    limit = nutritionLimit,
+                    level = 0.0f,
+                    logSize = 5
+                )
+            )
+        )
+
+        val mood = nutrimentLogValidationRepository.validate().first()
+
+        assertThat(mood).isEqualTo(Mood.GREEN)
+    }
+
+    @Test
+    fun emitGreenMoodOnNutrimentLogBelow80PercentageOfDayLimit() = runTest {
+        whenever(nutrimentLogRepository.getLogs()).thenReturn(
+            flowOf(
+                generateNutrimentLevelLog(
+                    limit = nutritionLimit,
+                    level = 0.79f,
+                    logSize = 5
+                )
+            )
+        )
+
+        val mood = nutrimentLogValidationRepository.validate().first()
+
+        assertThat(mood).isEqualTo(Mood.GREEN)
+    }
+
+    @Test
+    fun emitYellowMoodOnNutrimentLogOn80PercentOfDayLimit() = runTest {
+        whenever(nutrimentLogRepository.getLogs()).thenReturn(
+            flowOf(
+                generateNutrimentLevelLog(
+                    limit = nutritionLimit,
+                    level = 0.8f,
+                    logSize = 5
+                )
+            )
+        )
+
+        val mood = nutrimentLogValidationRepository.validate().first()
+
+        assertThat(mood).isEqualTo(Mood.YELLOW)
+    }
+
+    @Test
+    fun emitYellowMoodOnNutrimentLogOn99PercentOfDayLimit() = runTest {
+        whenever(nutrimentLogRepository.getLogs()).thenReturn(
+            flowOf(
+                generateNutrimentLevelLog(
+                    limit = nutritionLimit,
+                    level = 0.99f,
+                    logSize = 5
+                )
+            )
+        )
+
+        val mood = nutrimentLogValidationRepository.validate().first()
+
+        assertThat(mood).isEqualTo(Mood.YELLOW)
+    }
+
+    @Test
+    fun emitRedMoodOnNutrimentLogOnExceedingDayLimit() = runTest {
+        whenever(nutrimentLogRepository.getLogs()).thenReturn(
+            flowOf(
+                generateNutrimentLevelLog(
+                    limit = nutritionLimit,
+                    level = 1f,
+                    logSize = 10
+                )
+            )
+        )
+
+        val mood = nutrimentLogValidationRepository.validate().first()
+
+        assertThat(mood).isEqualTo(Mood.RED)
+    }
+}
