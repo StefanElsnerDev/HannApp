8a9222723dad9a7f0d0f228230d2de31f0700ee1
Stefan Elsner <stefan.elsner.dev@gmail.com>
2023-06-19 10:00:19 +0200

    add day quantity to MilkReferenceUiModel

--- app/src/main/java/com/example/hannapp/data/model/MilkLimitReferenceUiModel.kt
+++ app/src/main/java/com/example/hannapp/data/model/MilkLimitReferenceUiModel.kt
@@ -2,6 +2,7 @@ package com.example.hannapp.data.model
 
 data class MilkLimitReferenceUiModel(
     val total: String = "",
+    val day: String = "",
     val preNight: String = "",
     val night: String = ""
 )
