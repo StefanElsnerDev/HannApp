3842212d32eb09cc7bd5fdc912b1a79dffa9a17f
Stefan Elsner <stefan.elsner.dev@gmail.com>
2023-06-20 14:05:14 +0200

    add field width to Constraints

--- app/src/main/java/com/example/hannapp/ui/theme/Constraints.kt
+++ app/src/main/java/com/example/hannapp/ui/theme/Constraints.kt
@@ -4,6 +4,7 @@ import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 
 object Constraints {
+    val FIELD_WIDTH: Dp = 264.dp
     val PADDING: Dp = 15.dp
     val SPACE_VERTICAL: Dp = 32.dp
 }
