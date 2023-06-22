275178cba2fb6baa5f1a6d40f313319923b5b3be
Stefan Elsner <stefan.elsner.dev@gmail.com>
2023-06-22 13:38:42 +0200

    introduce composable reference content

---
+++ app/src/main/java/com/example/hannapp/ui/components/ReferenceContent.kt
@@ -0,0 +1,112 @@
+package com.example.hannapp.ui.components
+
+import android.content.res.Configuration
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import com.example.hannapp.R
+import com.example.hannapp.ui.theme.HannAppTheme
+import com.example.hannapp.ui.viewmodel.NutritionLimitContract
+
+@Composable
+fun ReferenceContent(
+    uiState: NutritionLimitContract.State,
+    event: (NutritionLimitContract.Event) -> Unit,
+    navController: NavHostController,
+    modifier: Modifier = Modifier
+) {
+    AppScaffold(
+        modifier = modifier.fillMaxSize(),
+        topBar = {
+            AppTopBar(
+                title = stringResource(id = R.string.nutrition_references)
+            ) {}
+        },
+        bottomBar = { NavigationBar(navController) },
+        floatingActionButton = {
+            IconButton(
+                onClick = {
+                    event(NutritionLimitContract.Event.OnValidate)
+                    if (uiState.isDataValid) event(NutritionLimitContract.Event.OnSave)
+                }
+            ) {
+                Icon(
+                    painter = painterResource(id = R.drawable.change),
+                    contentDescription = null,
+                    tint = MaterialTheme.colorScheme.secondary
+                )
+            }
+        }
+    ) {
+        Column(
+            modifier = modifier
+                .padding(it)
+                .fillMaxSize(),
+            verticalArrangement = Arrangement.SpaceEvenly,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            uiState.apply {
+                Surface(
+                    modifier = Modifier.wrapContentWidth(),
+                    shape = MaterialTheme.shapes.medium,
+                    color = MaterialTheme.colorScheme.surfaceVariant
+                ) {
+                    NutritionReferencesContent(
+                        kcalState = kcal,
+                        proteinState = protein,
+                        carbohydratesState = carbohydrates,
+                        fatState = fat,
+                        event = event
+                    )
+                }
+
+                Surface(
+                    modifier = Modifier.wrapContentWidth(),
+                    shape = MaterialTheme.shapes.medium,
+                    color = MaterialTheme.colorScheme.surfaceVariant
+                ) {
+                    MilkQuantityContent(
+                        totalState = totalQuantity,
+                        preNightState = preNightQuantity,
+                        nightState = nightQuantity,
+                        event = event
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Preview(
+    device = "spec:width=1280dp,height=800dp,dpi=240,orientation=landscape",
+    uiMode = Configuration.UI_MODE_NIGHT_NO
+)
+@Composable
+fun ReferencesContent_Preview() {
+    HannAppTheme {
+        ReferenceContent(
+            uiState = NutritionLimitContract.State(
+                kcal = NutritionLimitContract.ReferenceState.State(
+                    value = "112.3",
+                    isError = false
+                )
+            ),
+            navController = rememberNavController(),
+            event = {}
+        )
+    }
+}
