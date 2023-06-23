9eadfaff540a92fdb718d5c395cf05436f7f4ad8
Stefan Elsner <stefan.elsner.dev@gmail.com>
2023-06-23 15:56:44 +0200

    introduce ReferenceScreen

---
+++ app/src/main/java/com/example/hannapp/ui/screens/ReferenceScreen.kt
@@ -0,0 +1,26 @@
+package com.example.hannapp.ui.screens
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.navigation.NavHostController
+import com.example.hannapp.ui.components.ReferenceContent
+import com.example.hannapp.ui.viewmodel.NutritionLimitViewModel
+
+@Composable
+fun ReferenceScreen(
+    viewModel: NutritionLimitViewModel,
+    navController: NavHostController,
+    isCompactScreen: Boolean
+) {
+    val uiState by viewModel.state.collectAsStateWithLifecycle()
+
+    ReferenceContent(
+        uiState = uiState,
+        event = { viewModel.event(it) },
+        navController = navController,
+        modifier = Modifier.fillMaxSize()
+    )
+}
