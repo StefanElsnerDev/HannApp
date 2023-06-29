a1be90637aba421564c3cc66e3610b20473cc7ec
Stefan Elsner <stefan.elsner.dev@gmail.com>
2023-06-29 13:36:23 +0200

    add TODO for SubstitutionRepository

--- app/src/main/java/com/example/hannapp/data/repository/SubstitutionRepository.kt
+++ app/src/main/java/com/example/hannapp/data/repository/SubstitutionRepository.kt
@@ -5,5 +5,5 @@ import kotlinx.coroutines.flow.flowOf
 import javax.inject.Inject
 
 class SubstitutionRepository @Inject constructor() {
-    fun getMaltoDextrin() = flowOf(NutritionModel(kcal = 382.0))
+    fun getMaltoDextrin() = flowOf(NutritionModel(kcal = 382.0)) // TODO("introduce data source for maltodextrin nutriments")
 }
