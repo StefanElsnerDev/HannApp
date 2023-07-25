15a31db5ad9a34f4c78b611c7c82273438851f4a
Stefan Elsner <stefan.elsner.dev@gmail.com>
2023-07-25 21:44:35 +0200

    apply parameter to ProductSearchPagingSource when calling search via ProductSearchRepository

--- app/src/main/java/com/example/hannapp/data/repository/ProductSearchRepository.kt
+++ app/src/main/java/com/example/hannapp/data/repository/ProductSearchRepository.kt
@@ -1,22 +1,14 @@
 package com.example.hannapp.data.repository
 
-import androidx.paging.Pager
-import androidx.paging.PagingConfig
-import com.example.hannapp.data.source.ProductDataSource
 import com.example.hannapp.data.source.ProductSearchPagingSource
 import javax.inject.Inject
 
 class ProductSearchRepository @Inject constructor(
-    private val productDataSource: ProductDataSource
+    private val productSearchPagingSource: ProductSearchPagingSource
 ) {
 
-    fun search(searchString: String, pageSize: Int) = Pager(
-        PagingConfig(pageSize = pageSize)
-    ) {
-        ProductSearchPagingSource(
-            productDataSource = productDataSource,
-            searchString = searchString,
-            pageSize = pageSize
-        )
-    }.flow
+    fun search(searchString: String, pageSize: Int) = productSearchPagingSource.apply {
+        this.searchString = searchString
+        this.pageSize = pageSize
+    }
 }
--- app/src/test/java/com/example/hannapp/repository/ProductSearchRepositoryShould.kt
+++ app/src/test/java/com/example/hannapp/repository/ProductSearchRepositoryShould.kt
@@ -1,26 +1,39 @@
 package com.example.hannapp.repository
 
 import com.example.hannapp.data.repository.ProductSearchRepository
-import com.example.hannapp.data.source.ProductDataSource
+import com.example.hannapp.data.source.ProductSearchPagingSource
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
 import org.mockito.kotlin.mock
+import org.mockito.kotlin.whenever
 
 class ProductSearchRepositoryShould {
 
     private lateinit var productSearchRepository: ProductSearchRepository
-    private val productDataSource: ProductDataSource = mock()
+    private val productSearchPagingSource: ProductSearchPagingSource = mock()
+
+    private val searchString = "Apple"
+    private val pageSize = 24
 
     @BeforeEach
     fun beforeEach() {
+        whenever(productSearchPagingSource.searchString).thenReturn(searchString)
+        whenever(productSearchPagingSource.pageSize).thenReturn(pageSize)
+
         productSearchRepository = ProductSearchRepository(
-            productDataSource = productDataSource
+            productSearchPagingSource = productSearchPagingSource
         )
     }
 
-    /**
-     * TODO Add test cases
-     * Suggested implementation architecture
-     * @see [Android Developer Docs](https://developer.android.com/topic/libraries/architecture/paging/v3-paged-data#pagingdata-stream)
-     * complicates tests due to encapsulated Paging- and PagingSourceFactory-object
-     */
+    @Test
+    fun applySearchStringAndSizeToPagingSource() {
+        productSearchRepository.search(
+            searchString = searchString,
+            pageSize = pageSize
+        )
+
+        assertThat(productSearchPagingSource.searchString).isEqualTo(searchString)
+        assertThat(productSearchPagingSource.pageSize).isEqualTo(pageSize)
+    }
 }
