811d932f45a4b5ad4e9c7cead4628867c9d2013f
Stefan Elsner <stefan.elsner.dev@gmail.com>
2023-07-25 21:55:17 +0200

    return flow of paging data when calling search via GetProductSearchResultsCase

--- app/src/main/java/com/example/hannapp/domain/GetProductSearchResultsUseCase.kt
+++ app/src/main/java/com/example/hannapp/domain/GetProductSearchResultsUseCase.kt
@@ -1,11 +1,20 @@
 package com.example.hannapp.domain
 
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
 import com.example.hannapp.data.repository.ProductSearchRepository
 import javax.inject.Inject
 
 class GetProductSearchResultsUseCase @Inject constructor(
     private val productSearchRepository: ProductSearchRepository
 ) {
-    fun search(searchString: String, pageSize: Int) =
-        productSearchRepository.search(searchString, pageSize)
+
+    fun search(searchString: String, pageSize: Int) = Pager(
+        PagingConfig(pageSize = pageSize)
+    ) {
+        productSearchRepository.search(
+            searchString = searchString,
+            pageSize = pageSize
+        )
+    }.flow
 }
--- app/src/test/java/com/example/hannapp/usecase/GetProductSearchResultsCaseShould.kt
+++ app/src/test/java/com/example/hannapp/usecase/GetProductSearchResultsCaseShould.kt
@@ -1,15 +1,12 @@
 package com.example.hannapp.usecase
 
 import androidx.paging.PagingData
-import com.example.hannapp.data.model.api.Nutriments
-import com.example.hannapp.data.model.api.Product
 import com.example.hannapp.data.repository.ProductSearchRepository
+import com.example.hannapp.data.source.ProductSearchPagingSource
 import com.example.hannapp.domain.GetProductSearchResultsUseCase
-import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.first
-import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.test.runTest
-import org.junit.jupiter.api.Assertions
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.mockito.Mockito.mock
@@ -17,47 +14,16 @@ import org.mockito.kotlin.any
 import org.mockito.kotlin.verify
 import org.mockito.kotlin.whenever
 
-@OptIn(ExperimentalCoroutinesApi::class)
 class GetProductSearchResultsCaseShould {
 
     private lateinit var getProductSearchResultsUseCase: GetProductSearchResultsUseCase
     private val productSearchRepository: ProductSearchRepository = mock()
-
-    private val products = listOf(
-        Product(
-            code = "12345",
-            productName = "Apple",
-            nutriments = Nutriments(
-                1.1,
-                2.2,
-                3.3,
-                3.3,
-                4.4,
-                5.5,
-                6.6
-            )
-        ),
-        Product(
-            code = "6789",
-            productName = "Apple Green",
-            nutriments = Nutriments(
-                1.1,
-                2.2,
-                3.3,
-                3.3,
-                4.4,
-                5.5,
-                6.6
-            )
-        )
-    )
-
-    private val pagingData = PagingData.from(products)
+    private val productSearchPagingSource: ProductSearchPagingSource = mock()
 
     @BeforeEach
     fun beforeEach() {
         whenever(productSearchRepository.search(any(), any())).thenReturn(
-            flowOf(pagingData)
+            productSearchPagingSource
         )
 
         getProductSearchResultsUseCase = GetProductSearchResultsUseCase(
@@ -66,19 +32,16 @@ class GetProductSearchResultsCaseShould {
     }
 
     @Test
-    fun invokeSearchOfRepository() {
-        getProductSearchResultsUseCase.search("apple juice", 24)
+    fun invokeSearchOfRepository() = runTest {
+        getProductSearchResultsUseCase.search("apple juice", 24).first()
 
         verify(productSearchRepository).search(any(), any())
     }
 
     @Test
     fun emitSearchResults() = runTest {
-        val result = getProductSearchResultsUseCase.search("apple juice", 24)
+        val result = getProductSearchResultsUseCase.search("apple juice", 24).first()
 
-        Assertions.assertEquals(
-            pagingData,
-            result.first()
-        )
+        assertThat(result).isInstanceOf(PagingData::class.java)
     }
 }
