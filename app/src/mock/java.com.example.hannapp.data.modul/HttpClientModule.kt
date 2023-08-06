814164308ed0bf259a97a753576c5ead9a06e04d
Stefan Elsner <stefan.elsner.dev@gmail.com>
2023-08-06 14:40:38 +0200

    copy http client to mock flavor

---
+++ app/src/mock/java.com.example.hannapp.data.modul/HttpClientModule.kt
@@ -0,0 +1,43 @@
+import com.example.hannapp.data.remote.MockInterceptor
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object HttpClientModule {
+
+    @Singleton
+    @Provides
+    fun providesHttpLoggingInterceptor() = HttpLoggingInterceptor()
+        .apply {
+            level = HttpLoggingInterceptor.Level.BASIC
+        }
+
+    @Singleton
+    @Provides
+    fun providesMockInterceptor() = MockInterceptor()
+
+    @Singleton
+    @Provides
+    fun provideHttpClient(httpLoggingInterceptor: HttpLoggingInterceptor, mockInterceptor: MockInterceptor) =
+        OkHttpClient
+            .Builder()
+            .addInterceptor(mockInterceptor)
+            .addInterceptor(httpLoggingInterceptor)
+            .build()
+
+    @Singleton
+    @Provides
+    fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit = Retrofit.Builder()
+        .addConverterFactory(GsonConverterFactory.create())
+        .baseUrl("")
+        .client(okHttpClient)
+        .build()
+}
