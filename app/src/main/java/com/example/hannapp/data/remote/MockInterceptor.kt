c754e0c554f828e904bcb6029e756bac0b0b6101
Stefan Elsner <stefan.elsner.dev@gmail.com>
2023-08-06 15:25:59 +0200

    remove build type check from mock response

--- app/src/main/java/com/example/hannapp/data/remote/MockInterceptor.kt
+++ app/src/main/java/com/example/hannapp/data/remote/MockInterceptor.kt
@@ -1,28 +1,19 @@
 package com.example.hannapp.data.remote
 
-import com.example.hannapp.BuildConfig
 import okhttp3.Interceptor
 import okhttp3.MediaType.Companion.toMediaTypeOrNull
 import okhttp3.Response
 import okhttp3.ResponseBody.Companion.toResponseBody
 
 class MockInterceptor : Interceptor {
-    override fun intercept(chain: Interceptor.Chain): Response {
-        return if (BuildConfig.BUILD_TYPE == "mock") {
-            chain.proceed(
-                chain.request()
-            ).newBuilder()
-                .code(200)
-                .message(mockResponse)
-                .body(mockResponse.toResponseBody("application/json".toMediaTypeOrNull()))
-                .addHeader("content-type", "application/json")
-                .build()
-        } else {
-            chain.proceed(
-                chain.request()
-            )
-        }
-    }
+    override fun intercept(chain: Interceptor.Chain): Response = chain.proceed(
+        chain.request()
+    ).newBuilder()
+        .code(200)
+        .message(mockResponse)
+        .body(mockResponse.toResponseBody("application/json".toMediaTypeOrNull()))
+        .addHeader("content-type", "application/json")
+        .build()
 }
 
 const val mockResponse =
