a7ce1d45d39ddbfd59d417fc245ad22e70716e08
Stefan Elsner <stefan.elsner.dev@gmail.com>
2023-08-09 17:16:50 +0200

    introduce mock api for product search to mock flavor

---
+++ app/src/mock/java/com/example/hannapp/data/remote/ProductSearchApi.kt
@@ -0,0 +1,15 @@
+package com.example.hannapp.data.remote
+
+import com.example.hannapp.data.model.api.ProductSearchResult
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface ProductSearchApi {
+    @GET("get")
+    suspend fun search(
+        @Query("search_terms") searchString: String,
+        @Query("page") page: Int,
+        @Query("page_size") pageSize: Int = 24
+    ): Response<ProductSearchResult>
+}
