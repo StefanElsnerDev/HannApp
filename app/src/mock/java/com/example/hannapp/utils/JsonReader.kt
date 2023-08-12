f739c46d9fcdab55c3374d977e6474abf756d28b
Stefan Elsner <stefan.elsner.dev@gmail.com>
2023-08-12 14:08:17 +0200

    remove injection of context into json reader

--- app/src/mock/java/com/example/hannapp/utils/JsonReader.kt
+++ app/src/mock/java/com/example/hannapp/utils/JsonReader.kt
@@ -1,11 +1,9 @@
 package com.example.hannapp.utils
 
 import android.content.Context
-import dagger.hilt.android.qualifiers.ApplicationContext
-import javax.inject.Inject
 
-class JsonReader @Inject constructor(
-    @ApplicationContext private val context: Context
+class JsonReader(
+    private val context: Context
 ) {
     fun jsonToString(fileName: String): String = context.assets.open(fileName)
         .bufferedReader()
